8030afc7c3e6ca7924fc560e4ec699c372520b12
dockerlead <dockerlead@gmail.com>
2020-12-07 03:35:14 -0600

    :construction:Format chuck and get back the dev env

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -1,18 +1,35 @@
-from fastapi import WebSocket, APIRouter
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from app.services.iqoption_service import *
 import json
 import time
 
-router = APIRouter()
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+
+router = APIRouter()
+manager = ConnectionManager()
 start_candles_stream()
 
 
 @router.websocket_route("/ws")
 async def echo_socket(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        chunk = json.dumps(get_real_time_candles())
-        await websocket.send_json(chunk)
-        time.sleep(1)
+    await manager.connect(websocket)
+    try:
+        while True:
+            chunk = get_real_time_candles()
+            for item in chunk:
+                await websocket.send_json(item)
+            time.sleep(1)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
--- app/services/iqoption_service.py
+++ app/services/iqoption_service.py
@@ -1,9 +1,10 @@
 from iqoptionapi.stable_api import IQ_Option
+from datetime import datetime
 import os
 
 goal="EURUSD"
 size=10#size=[1,5,10,15,30,60,120,300,600,900,1800,3600,7200,14400,28800,43200,86400,604800,2592000,"all"]
-maxdict=3
+maxdict=1
 I_want_money=IQ_Option(os.getenv("IQ_OPTION_USER"), os.getenv("IQ_OPTION_PASSWORD"))
 
 def start_candles_stream():
@@ -17,7 +18,26 @@ def get_real_time_candles():
     Once you got a chunk by this function, you need to call `time.sleep(1)`
     """
     candles=I_want_money.get_realtime_candles(goal,size)
-    return candles
+    inputs = []
+    input = {
+        'open': 0,
+        'high': 0,
+        'low': 0,
+        'close': 0,
+        'volume': 0,
+        'timestamp': ''
+    }
+    for timestamp in candles:
+        input["open"]      = candles[timestamp]["open"]
+        input["high"]      = candles[timestamp]["max"]
+        input["low"]       = candles[timestamp]["min"]
+        input["close"]     = candles[timestamp]["close"]
+        input["volume"]    = candles[timestamp]["volume"]
+        input["timestamp"] = datetime.utcfromtimestamp(timestamp).strftime("%m/%d/%Y, %H:%M:%S")
+        inputs.append(input)
+    return inputs    
+
 
 def stop_candles_stream():
     I_want_money.stop_candles_stream(goal,size)
+
